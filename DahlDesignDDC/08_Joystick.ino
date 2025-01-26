e9449fcbc598d9127fe9ca9a32c3d4baf17e5b03
igr95 <22528963+igr95@users.noreply.github.com>
2025-01-26 16:58:27 +0100

    488 v1

--- DahlDesignDDC/08_Joystick.ino
+++ DahlDesignDDC/08_Joystick.ino
@@ -2,20 +2,20 @@
 //-------JOYSTICK--------
 //-----------------------
 
-#define BUTTONCOUNT 64
+#define BUTTONCOUNT 16
 #define HATCOUNT 0
 
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_JOYSTICK,
     BUTTONCOUNT,   //Button count
     HATCOUNT,      //Hat switch count
     true,          //X axis, used for clutch
-    true,          //Y axis, used for button mode field
-    true,          //Z axis, used for encoder mode field
+    false,          //Y axis, used for button mode field
+    false,          //Z axis, used for encoder mode field
     false,         //Rx axis 
-    true,          //Ry axis, used for bite point posting
+    false,          //Ry axis, used for bite point posting
     false,         //Rz axis 
     false,         //Rudder
-    true,          //Throttle, used for dual clutch mode 4
+    false,          //Throttle, used for dual clutch mode 4
     false,         //Accelerator
-    true,          //Brake, used for dual clutch mode 4
+    false,          //Brake, used for dual clutch mode 4
     false);        //Steering
--- DahlDesignDDC/30_Switches.ino
+++ DahlDesignDDC/30_Switches.ino
@@ -2,10 +2,59 @@
   //---------SWITCHES START HERE----------
   //--------------------------------------
 
+  // TABLE INJECT
 
+  // paddles (down - up)
+  switchTableInject(17, 1, 1);  // 0
+  switchTableInject(22, 1, 2); // 1
 
+  // buttons (top-bottom)
+  switchTableInject(0, 1, 3); // 2
+  switchTableInject(1, 1, 7); // 3
+  switchTableInject(2, 1, 6); // 4
+  switchTableInject(3, 1, 5); // 5
+  switchTableInject(4, 1, 4); // 6
+  // buttons right (top-bottom)
+  switchTableInject(11, 1, 10); // 7
+  switchTableInject(12, 1, 11); // 8
+  switchTableInject(13, 1, 8); // 9 
+  switchTableInject(14, 1, 9); // 10
+  switchTableInject(15, 1, 12); // 11
 
+  // rotaries (left - right)
+  switchTableInject(20, 1, 13);
+  switchTableInject(21, 1, 14);
+  switchTableInject(18, 1, 15);
+  switchTableInject(19, 1, 16);
 
+  // ASSIGNMENT
+
+  // paddles
+  pushButton(1, 1);
+  pushButton(1, 2);
+
+  // buttons left
+  pushButton(1, 3);
+  pushButton(1, 4);
+  pushButton(1, 5);
+  pushButton(1, 6);
+  pushButton(1, 7);
+  // buttons right
+  pushButton(1, 8);
+  pushButton(1, 9);
+  pushButton(1, 10);
+  pushButton(1, 11);
+  pushButton(1, 12);
+
+  // rotaries
+  funkyRotary(1, 13, 14, false);
+  funkyRotary(1, 15, 16, false);
+
+  // clutch (channel 3: ADC3 -A2-)
+  singleClutch(3, 17000, 29000);
+  
+  // top left and top right white buttons, 8 seconds
+  firmwareUploadMode(1, 3, 1, 8, 8000);
 
   //--------------------------------------
   //---------SWITCHES END HERE------------
