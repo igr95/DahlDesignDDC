fe6584b900dfbe24ea084fd0bb43eb45e2f7411b
igr95 <22528963+igr95@users.noreply.github.com>
2025-10-19 09:26:10 +0200

    chore: 95SR Shield PCB

--- DahlDesignDDC/10_TableAndAnalog.ino
+++ DahlDesignDDC/10_TableAndAnalog.ino
@@ -7,7 +7,7 @@
 uint8_t row[] = { 99 };
 const uint8_t rowCount = sizeof(row) / sizeof(row[0]);
 
-uint8_t col[] = { 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99 };
+uint8_t col[] = { 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99 };
 const uint8_t colCount = sizeof(col) / sizeof(col[0]);
 
 //---------------------------------------
@@ -16,7 +16,7 @@ const uint8_t colCount = sizeof(col) / sizeof(col[0]);
 
 uint8_t buttonNumber[rowCount][colCount] =
 {
-  { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 },
+  { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 },
 };
 
 //---------------------------------------
@@ -25,11 +25,16 @@ uint8_t buttonNumber[rowCount][colCount] =
 
 
 uint8_t directPins[] = { 
-  8, 9, 10, 11, // rotaries
-  12, 13, 14, 15, // rotaries
-  6, 7, //paddles
-  0, 1, 2, 3, 4, // buttons
-  21, 20, 19, 18, 17, // buttons
+  0, // Xtra Left
+  1, 2, 3, 4, 5, // buttons left
+  6, 7, // ROT1
+  8, 9, // ROT2
+  10, 11, // ROT3
+  12, 13, // ROT4
+  14, // Xtra Right
+  15, 22, 21, 20, 19, // buttons right
+  17, // shifter left
+  18, // shifter right
 };
 const uint8_t directPinsCount = sizeof(directPins) / sizeof(directPins[0]);
 
@@ -53,10 +58,10 @@ uint8_t analogPins[analogChannelCount] =                 //Pin number for your a
 { ADC1, ADC2, ADC3 };
 
 uint8_t analogButtonNumber[analogChannelCount] =         //Button number for your analog channels
-{ 0, 0, 0 };
+{ 24, 0, 0 };
 
 uint8_t analogButtonNumberIncMode[analogChannelCount] =  //Button number for alternate modes
-{ 0, 0, 0 };
+{ 22, 0, 0 };
 
 
 
--- DahlDesignDDC/30_Switches.ino
+++ DahlDesignDDC/30_Switches.ino
@@ -5,26 +5,26 @@
   // TABLE INJECT
 
   // paddles (down - up)
-  switchTableInject(6, 1, 1); // 0
-  switchTableInject(7, 1, 2); // 1
+  switchTableInject(0, 1, 1); // 0
+  switchTableInject(18, 1, 2); // 1
 
   // buttons (top-bottom)
-  switchTableInject(0, 1, 3); // 2
-  switchTableInject(1, 1, 4); // 3
-  switchTableInject(2, 1, 5); // 4
-  switchTableInject(3, 1, 6); // 5
-  switchTableInject(4, 1, 7); // 6
+  switchTableInject(1, 1, 3); // 2
+  switchTableInject(2, 1, 4); // 3
+  switchTableInject(3, 1, 5); // 4
+  switchTableInject(4, 1, 6); // 5
+  switchTableInject(5, 1, 7); // 6
 
   // buttons right (top-bottom)
-  switchTableInject(21, 1, 8);  // 7
-  switchTableInject(20, 1, 9);  // 8
-  switchTableInject(19, 1, 10); // 9 
-  switchTableInject(18, 1, 11); // 10
-  switchTableInject(17, 1, 12); // 11
+  switchTableInject(15, 1, 8);  // 7
+  switchTableInject(22, 1, 9);  // 8
+  switchTableInject(21, 1, 10); // 9 
+  switchTableInject(20, 1, 11); // 10
+  switchTableInject(19, 1, 12); // 11
 
   // rotaries (left - right)
-  switchTableInject(14, 1, 13);
-  switchTableInject(15, 1, 14);
+  switchTableInject(6, 1, 13);
+  switchTableInject(7, 1, 14);
   switchTableInject(8, 1, 15);
   switchTableInject(9, 1, 16);
   switchTableInject(10, 1, 17);
@@ -32,9 +32,13 @@
   switchTableInject(12, 1, 19);
   switchTableInject(13, 1, 20);
 
+  // xtra left-right
+  // switchTableInject(17, 1, 21); // 2
+  // switchTableInject(14, 1, 22); // 2
+
   // ASSIGNMENT
 
-  // paddles
+  // 2 paddles
   pushButton(1, 1);
   pushButton(1, 2);
 
@@ -44,6 +48,7 @@
   pushButton(1, 5);
   pushButton(1, 6);
   pushButton(1, 7);
+
   // buttons right
   pushButton(1, 8);
   pushButton(1, 9);
@@ -52,14 +57,22 @@
   pushButton(1, 12);
 
   // rotaries
-  E18Inc(1, 13, false);
+  // E18Inc(1, 13, false);
   E18Inc(1, 15, false);
   E18Inc(1, 17, false);
   E18Inc(1, 19, false);
 
+  // xtra left-right
+  // pushButton(1, 21);
+  // pushButton(1, 22);
+
+  // grayhill
+  rotaryAnalog2Mode(1, 1, 100, 2950, 5570, 8200, 10800, 13410, 16180, 19140, 22200, 25540, 28980, 32655, false);
+  // setAnalogSwitchMode(1, 1, 1); // change rotary to incremental mode
+
   // clutch (channel 3: ADC2, channel 2: ADC1)
-  dualClutch(2, 9500, 6550, 3, 10800, 9700, false);
-  // checkValue(2);
+  dualClutch(2, 9000, 5600, 3, 11000, 8600, false);
+  // checkValue(1);
   // checkValue(3);
 
   // top left and top right white buttons, 8 seconds
